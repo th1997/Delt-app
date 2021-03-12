6ab6054de8665054697ab54264bbd674966ab37a
théo bonhomme <teo59460@gmail.com>
2021-03-12 14:05:59 +0100

    Ajout OCR ( non fonctionnel )

--- app/src/main/java/com/example/projetl3/MainActivity.kt
+++ app/src/main/java/com/example/projetl3/MainActivity.kt
@@ -1,24 +1,38 @@
 package com.example.projetl3
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.Manifest
+import android.app.ProgressDialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import java.util.concurrent.Executors
-import androidx.camera.core.*
-import androidx.camera.lifecycle.ProcessCameraProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
-import java.net.URI
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.io.InputStream
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+
 typealias LumaListener = (luma: Double) -> Unit
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +40,42 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
+
+    //---------------------OCR-------------------------------
+    /*private var mTessOCR: TessOCR? = null
+    private var mResult: TextView? = null
+    private var mProgressDialog: ProgressDialog? = null
+    private var mImage: ImageView? = null
+    private var mButtonGallery: Button? = null
+    private  var mButtonCamera:Button? = null
+    private val mCurrentPhotoPath: String? = null
+    private val REQUEST_TAKE_PHOTO = 1
+    private val REQUEST_PICK_PHOTO = 2
+
+    private fun uriOCR(uri: Uri?) {
+        if (uri != null) {
+            var `is`: InputStream? = null
+            try {
+                `is` = contentResolver.openInputStream(uri)
+                val bitmap = BitmapFactory.decodeStream(`is`)
+                mImage!!.setImageBitmap(bitmap)
+                doOCR(bitmap)
+            } catch (e: FileNotFoundException) {
+                // TODO Auto-generated catch block
+                e.printStackTrace()
+            } finally {
+                if (`is` != null) {
+                    try {
+                        `is`.close()
+                    } catch (e: IOException) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    }*/
+    //--------------------------------------------------------------
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,11 +86,19 @@ class MainActivity : AppCompatActivity() {
             startCamera()
         } else {
             ActivityCompat.requestPermissions(
-                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
         }
+        //--------------------OCR-----------------------------
+        /*mResult = findViewById<View>(R.id.ocr_text) as TextView
+        val mButtonCamera = findViewById<View>(R.id.camera_capture_button) as Button
+        //mButtonCamera.setOnClickListener(this)
+        mTessOCR = TessOCR()*/
+        //-----------------------------------------------------
 
         // Set up the listener for take photo button
         camera_capture_button.setOnClickListener { takePhoto() }
+
 
         // Set up the listener for button login page
         login_button.setOnClickListener { switchToLoginPage() }
@@ -48,6 +106,8 @@ class MainActivity : AppCompatActivity() {
         outputDirectory = getOutputDirectory()
 
         cameraExecutor = Executors.newSingleThreadExecutor()
+
+
     }
 
     private fun switchToLoginPage(){
@@ -63,9 +123,11 @@ class MainActivity : AppCompatActivity() {
 
         // Create time-stamped output file to hold the image
         val photoFile = File(
-            outputDirectory ,
-            SimpleDateFormat(FILENAME_FORMAT, Locale.US
-            ).format(System.currentTimeMillis()) + ".jpg")
+            outputDirectory,
+            SimpleDateFormat(
+                FILENAME_FORMAT, Locale.US
+            ).format(System.currentTimeMillis()) + ".jpg"
+        )
 
         // Create output options object which contains file + metadata
         val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
@@ -74,14 +136,16 @@ class MainActivity : AppCompatActivity() {
         // been taken
         var savedUri: Uri;
         imageCapture.takePicture(
-            outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
                 override fun onError(exc: ImageCaptureException) {
                     Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
                 }
 
                 override fun onImageSaved(output: ImageCapture.OutputFileResults) {
                     savedUri = Uri.fromFile(photoFile)
-                    val msg = "Photo capture succeeded: $savedUri"
+                    val msg = "Photo enregistrée( $savedUri )"
                     Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
                     Log.d(TAG, msg)
                 }
@@ -114,9 +178,10 @@ class MainActivity : AppCompatActivity() {
 
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture)
+                    this, cameraSelector, preview, imageCapture
+                )
 
-            } catch(exc: Exception) {
+            } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
 
@@ -125,7 +190,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
-            baseContext, it) == PackageManager.PERMISSION_GRANTED
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun getOutputDirectory(): File {
@@ -142,19 +208,43 @@ class MainActivity : AppCompatActivity() {
 
     override fun onRequestPermissionsResult(
         requestCode: Int, permissions: Array<String>, grantResults:
-        IntArray) {
+        IntArray
+    ) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
                 startCamera()
             } else {
-                Toast.makeText(this,
+                Toast.makeText(
+                    this,
                     "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT).show()
+                    Toast.LENGTH_SHORT
+                ).show()
                 finish()
             }
         }
     }
 
+    //------------------------------OCR---------------------------
+    /*private fun doOCR(bitmap: Bitmap) {
+        if (mProgressDialog == null) {
+            mProgressDialog = ProgressDialog.show(
+                this, "Processing",
+                "Doing OCR...", true
+            )
+        } else {
+            mProgressDialog!!.show()
+        }
+        Thread {
+            val result: String = mTessOCR!!.getOCRResult(bitmap)
+            runOnUiThread(Runnable { // TODO Auto-generated method stub
+                if (result != null && result != "") {
+                    mResult?.setText(result)
+                }
+                mProgressDialog?.dismiss()
+            })
+        }.start()
+    }*/
+    //-------------------------------------------------------------------
     companion object {
         private const val TAG = "CameraXBasic"
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
