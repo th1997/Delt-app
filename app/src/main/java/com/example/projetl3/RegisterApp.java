162de6d9226b2a716bf8e5176451a9889503585f
Constant Abrh <constant.abrh@gmail.com>
2021-05-16 15:01:44 +0200

    Mise à jour RegisterApp.java Changement google-services.json Ajout dans build.gradle et app/build.graddle

--- app/src/main/java/com/example/projetl3/RegisterApp.java
+++ app/src/main/java/com/example/projetl3/RegisterApp.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,12 +14,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterApp extends AppCompatActivity {
 
@@ -30,6 +33,7 @@ public class RegisterApp extends AppCompatActivity {
     private TextView userLoginPage;
     private ProgressBar progressBar;
     private FirebaseAuth mAuth;
+    private final FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,8 +79,54 @@ public class RegisterApp extends AppCompatActivity {
         progressBar = (ProgressBar)findViewById(R.id.progressBar);
     }
 
-    private boolean register(){
-        boolean isRegistered = false;
+    private void register(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Users");
+        myRef.setValue("Hello, World!").addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(RegisterApp.this, "Passe tranquille #1", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(RegisterApp.this, "Passe pas tranquille de fou #1", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull @NotNull Exception e) {
+                Toast.makeText(RegisterApp.this, e.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        /*
+        DatabaseReference ref = database.getReference();
+        DatabaseReference usersRef = ref.child("users");
+        Map<String, Object> users = new HashMap<>();
+        users.put("abc", new Utilisateur("constant@gmail.com", "Abraham", "Constant"));
+        Toast.makeText(RegisterApp.this, "Passe tranquille #1! Ref = " + ref.toString() + " | Db = " + database.toString(), Toast.LENGTH_LONG).show();
+        usersRef.setValue(users).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull @NotNull Exception e) {
+                Toast.makeText(RegisterApp.this, "Erreur = " + e.toString(), Toast.LENGTH_LONG).show();
+            }
+        }); .addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<Void> task) {
+                Toast.makeText(RegisterApp.this, "Passe tranquille #2! Ref = " + ref.toString() + " | Db = " + database.toString(), Toast.LENGTH_LONG).show();
+                if(task.isSuccessful()){
+                    Toast.makeText(RegisterApp.this, "Passe tranquille #3! Ref = " + ref.toString() + " | Db = " + database.toString(), Toast.LENGTH_LONG).show();
+                } else if(task.isComplete()){
+                    Toast.makeText(RegisterApp.this, "Passe tranquille #4! Ref = " + ref.toString() + " | Db = " + database.toString(), Toast.LENGTH_LONG).show();
+                } else if(task.isCanceled()){
+                    Toast.makeText(RegisterApp.this, "Passe tranquille #5! Ref = " + ref.toString() + " | Db = " + database.toString(), Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(RegisterApp.this, "Passe tranquille #6! Ref = " + ref.toString() + " | Db = " + database.toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        */
+        /*
         String emailText = email.getText().toString();
         String nomText = nom.getText().toString();
         String prenomText = prenom.getText().toString();
@@ -86,64 +136,51 @@ public class RegisterApp extends AppCompatActivity {
         if(emailText.isEmpty()){
             email.setError("Veuillez remplir ce champ");
             email.requestFocus();
-            return false;
         } else if(!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()){
             email.setError("Email invalide");
             email.requestFocus();
-            return false;
         } else if(nomText.isEmpty()){
             nom.setError("Veuillez remplir ce champ");
             nom.requestFocus();
-            return false;
         } else if(prenomText.isEmpty()){
             prenom.setError("Veuillez remplir ce champ");
             prenom.requestFocus();
-            return false;
         } else if(pwd1Text.isEmpty()){
             pwd1.setError("Veuillez remplir ce champ");
             pwd1.requestFocus();
-            return false;
         } else if(pwd1Text.length() < 8){
             pwd1.setError("Le mot de passe doit contenir au moins 8 caractères");
         } else if(pwd2Text.isEmpty()){
             pwd2.setError("Veuillez remplir ce champ");
             pwd2.requestFocus();
-            return false;
         } else if(!pwd1Text.equalsIgnoreCase(pwd2Text)){
             pwd2.setError("Les mot de passe doivent correspondre");
             pwd2.requestFocus();
         }  else {
             progressBar.setVisibility(View.VISIBLE);
+
             mAuth.createUserWithEmailAndPassword(emailText, pwd1Text)
                     .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
                             if(task.isSuccessful()){
                                 Utilisateur user = new Utilisateur(emailText, nomText, prenomText);
-                                FirebaseDatabase.getInstance().getReference("Utilisateurs")
-                                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                        .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                    @Override
-                                    public void onComplete(@NonNull @NotNull Task<Void> task) {
-                                        if(task.isSuccessful()){
-                                            Toast.makeText(RegisterApp.this, "L'utilisateur à bien été enregistré!", Toast.LENGTH_LONG).show();
-                                            progressBar.setVisibility(View.GONE);
-                                            // redirect to main
-                                        } else {
-                                            Toast.makeText(RegisterApp.this, "Erreur lors de l'inscription, veuillez réessayer! #1", Toast.LENGTH_LONG).show();
-                                        }
-                                    }
-                                });
+                                Toast.makeText(RegisterApp.this, "L'utilisateur à bien été enregistré #1! " + user.toString(), Toast.LENGTH_LONG).show();
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                DatabaseReference myRef = database.getReference();
+                                Toast.makeText(RegisterApp.this, "Ref= " + myRef.toString(), Toast.LENGTH_LONG).show();
+
                             } else {
                                 Toast.makeText(RegisterApp.this, "Erreur lors de l'inscription, veuillez réessayer! #2", Toast.LENGTH_LONG).show();
+                                progressBar.setVisibility(View.GONE);
                             }
                         }
                     });
+
+
                     progressBar.setVisibility(View.GONE);
 
-        }
-
-        return isRegistered;
+        }*/
     }
 
 }
